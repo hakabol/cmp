bf568b06b0ec479ef74a4b1ec38015c416715e2a
hakabol <habelcijuanu@gmail.com>
2026-06-28 18:57:23 +0530

    it now find all good files in a given repo

--- model/scraper/src/main.rs
+++ model/scraper/src/main.rs
@@ -1,4 +1,23 @@
+use std::{ffi::OsStr, fs, path::PathBuf, vec};
+
 use serde::Serialize;
+
+const NEEDED_EXT: &[&str] = &[
+    "rs",
+    "toml",
+    "lock",
+    "json",
+    "yaml",
+    "yml",
+    "ron",
+    "md",
+    "txt",
+    "sh",
+    "bash",
+    "nix",
+    "py",
+    "ipynb"
+];
 
 #[derive(Serialize)]
 struct Chunk {
@@ -20,5 +39,53 @@ struct Repo {
 }
 
 fn main() {
-    println!("Hello, world!");
+    let path = clone_url("https://github.com/BurntSushi/ripgrep").unwrap();
+
+    let files = walk_dirs(path);
+
+    for file in files{
+        println!("{}", file.display());
+    }
+
+    fs::remove_dir_all("temp").unwrap();
+}
+
+fn clone_url(url: &str) -> anyhow::Result<PathBuf>{
+    let repo_name = url
+        .split('/')
+        .last()
+        .unwrap()
+        .trim_end_matches(".git");
+
+    let path = PathBuf::from("temp").join(repo_name);
+
+    std::fs::create_dir_all("temp")?;
+
+    git2::Repository::clone(url, &path)?;
+
+    Ok(path)
+}
+
+fn walk_dirs(path: PathBuf) -> Vec<PathBuf>{
+    let mut files: Vec<PathBuf> = vec![];
+
+    for entry in fs::read_dir(path).unwrap(){
+        let entry_idk = entry.unwrap();
+        let sub_path = entry_idk.path();
+
+        if sub_path.is_dir(){
+            let mut sub_files = walk_dirs(sub_path);
+
+            files.append(&mut sub_files);
+        }
+        else{
+            if let Some(ext) = sub_path.extension().unwrap_or(OsStr::new("")).to_str(){
+                if NEEDED_EXT.contains(&ext){
+                    files.push(sub_path)
+                }
+            }
+        }
+    }
+
+    files
 }
